145adcf7b847cb3b4b5ca75386872db253b96cb5
François Baury <fbaury@laposte.net>
2022-07-06 05:49:53 +0200

    session ou local ou rien

--- main.js
+++ main.js
@@ -3,13 +3,16 @@ let initPourProd = () => {
   sessionStorage.setItem("1", "c'est le père noël !!!");
   sessionStorage.setItem("2", "hello world !!!");
 }
-
+let init2 = () => {
+	localStorage.setItem("0", "ho pla");
+	localStorage.setItem("1", "Boum !!!");
+	}
 let build = (datas = []) => {
   if (datas.length > 0) {
     document.getElementsByTagName('form')[0].removeChild(document.getElementsByTagName('article')[0]);
     for (let i = 0; i < datas.length; i++) {
     	elt.addAZone(datas[i], i);
-    	document.getElementsByTagName('textArea')[i].innerHTML = sessionStorage.getItem(i);
+    	document.getElementsByTagName('textArea')[i].innerHTML = datas[i];
     }
     
   }
@@ -17,7 +20,14 @@ let build = (datas = []) => {
 //fonction listen sur tous les contrôles
 //si un contrôle est activé --> on affiche les contrôle et on lance une fonction listen sur tous les sous contrôle
 
-initPourProd();
-console.log(reg.getStorageTab(true));
+//initPourProd();
+//init2();
+
 //lancer build avec session si session>0 sinon local si local == 0 on ne fait rien
-build(reg.getStorageTab(true));
+let sessionOrLocal = () => {
+	if (sessionStorage.length > 0) return true;
+	if (localStorage.length > 0) return false;
+	
+	return "no-response";
+}
+build(reg.getStorageTab(sessionOrLocal()));
